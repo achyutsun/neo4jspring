ebd95edb3a0af7984dc4385b097b89c101fbac39
Nagulapalle, Rajender <rnagulapalle@paypal.com>
2015-09-11 20:19:22 -0700

    fixed org.springframework.data.mapping.PersistentPropertyAccessor issue

--- src/main/java/movies/spring/data/neo4j/MyNeo4jConfiguration.java
+++ src/main/java/movies/spring/data/neo4j/MyNeo4jConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.core.TypeRepresentationStrategy;
-import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+import org.springframework.data.neo4j.rest.SpringCypherRestGraphDatabase;
 import org.springframework.data.neo4j.support.typerepresentation.NoopRelationshipTypeRepresentationStrategy;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -31,9 +31,11 @@ public class MyNeo4jConfiguration extends Neo4jConfiguration {
 
     public static final String URL = System.getenv("NEO4J_URL") != null ? System.getenv("NEO4J_URL") : "http://localhost:7474/db/data/";
 
-    @Bean
+    @SuppressWarnings("deprecation")
+	@Bean
     public GraphDatabaseService graphDatabaseService() {
-        return new SpringRestGraphDatabase(URL);
+//        return new SpringRestGraphDatabase(URL, "neo4j", "admin");
+    	return new SpringCypherRestGraphDatabase("http://app41193919.sb02.stations.graphenedb.com:24789/db/data/", "app41193919", "0VcakXpduIrPJdpdBWMr");
     }
 
     @Override
--- src/main/java/movies/spring/data/neo4j/SampleMovieApplication.java
+++ src/main/java/movies/spring/data/neo4j/SampleMovieApplication.java
@@ -1,29 +1,18 @@
 package movies.spring.data.neo4j;
 
-import movies.spring.data.neo4j.repositories.MovieRepository;
-import movies.spring.data.neo4j.services.MovieService;
-import org.neo4j.graphdb.GraphDatabaseService;
+import java.io.IOException;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.config.Neo4jConfiguration;
-import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
+import movies.spring.data.neo4j.services.MovieService;
 
 /**
  * @author mh
